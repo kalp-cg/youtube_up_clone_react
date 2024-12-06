50c63d5719253bac6cb207c3727b961c9728e8e2
kalp patel <kalp.patel.codinggita@gmail.com>
2024-12-06 11:52:52 +0530

    updated

--- vite_app/src/Videosection.jsx
+++ vite_app/src/Videosection.jsx
@@ -39,7 +39,7 @@ const videos = [
 
 const videos1 = [
     {
-        thumbnail: "https://github.com/PatelNeelMahesh/frontend_tasks/blob/main/02.youtube-clone/assets/Thumbnail-4.png?raw=true",
+        thumbnail: "https://github.com/PatelNeelMahesh/frontend_tasks/blob/main/02.youtube-clone/assets/Thumbnail-4.png?raw=true" ,
         playButton: "https://github.com/PatelNeelMahesh/frontend_tasks/blob/main/02.youtube-clone/assets/Ellipse%201%20(1).png?raw=true",
         title: "Title of the Video",
         views: "12,000,000 views",
@@ -117,7 +117,7 @@ function Videosection() {
     return (
         <>
             <div className="all">
-                    
+
 
 
 
@@ -126,18 +126,27 @@ function Videosection() {
                     <div className="row">
                         {videos.map((video, index) => (
                             <div className="video" key={index}>
-                                <img src={video.thumbnail} alt="Video Thumbnail" />
+                                <img style={{
+                                    width:'370px',
+
+                                }} src={video.thumbnail } alt="Video Thumbnail" />
                                 <div className="cpy">
                                     <div className="play-btn">
-                                        <img src={video.playButton} alt="Play Button" />
+                                        <img style={
+                                            {
+                                                width:'45px',
+                                                height:'45px',
+                                                marginTop:'.5vh'
+                                            }
+                                        } src={video.playButton} alt="Play Button" />
                                     </div>
                                     <div className="cpy2">
                                         <div className="title">{video.title}</div>
                                         <div className="channel">{video.channel}</div>
-                                        <div className="cpy3">
+                                        {/* <div className="cpy1"> */}
                                             <div className="views">{video.views}</div>
                                             <div className="date">{video.date}</div>
-                                        </div>
+                                        {/* </div> */}
                                     </div>
                                 </div>
                             </div>
@@ -149,14 +158,26 @@ function Videosection() {
                     <div className="row">
                         {videos1.map((video, index) => (
                             <div className="video" key={index}>
-                                <img src={video.thumbnail} alt="Video Thumbnail" />
+                                <img  style={{
+                                    width:'370px',
+                                    
+                                }} src={video.thumbnail} alt="Video Thumbnail" />
                                 <div className="play-btn">
-                                    <img src={video.playButton} alt="Play Button" />
+                                    <img style={
+                                            {
+                                                width:'45px',
+                                                height:'45px',
+                                                marginTop:'.5vh'
+                                            }
+                                        } src={video.playButton} alt="Play Button" />
                                 </div>
-                                <div className="title">{video.title}</div>
-                                <div className="views">{video.views}</div>
+                                <div className="title1">{video.title}</div>
                                 <div className="channel">{video.channel}</div>
+                                {/* <div className='cpy2'> */}
+                                <div className="views">{video.views}</div>
+                            
                                 <div className="date">{video.date}</div>
+                                {/* </div> */}
                             </div>
                         ))}
                     </div>
@@ -166,14 +187,26 @@ function Videosection() {
                     <div className="row">
                         {videos2.map((video, index) => (
                             <div className="video" key={index}>
-                                <img src={video.thumbnail} alt="Video Thumbnail" />
+                                <img  style={{
+                                    width:'370px',
+                                    
+                                }} src={video.thumbnail} alt="Video Thumbnail" />
                                 <div className="play-btn">
-                                    <img src={video.playButton} alt="Play Button" />
+                                    <img style={
+                                            {
+                                                width:'45px',
+                                                height:'45px',
+                                                marginTop:'.5vh'
+                                            }
+                                        } src={video.playButton} alt="Play Button" />
                                 </div>
-                                <div className="title">{video.title}</div>
-                                <div className="views">{video.views}</div>
+                                <div className="title2">{video.title}</div>
                                 <div className="channel">{video.channel}</div>
+                                {/* <div className='cpy3'> */}
+                                <div className="views">{video.views}</div>
+                              
                                 <div className="date">{video.date}</div>
+                                {/* </div> */}
                             </div>
                         ))}
                     </div>
